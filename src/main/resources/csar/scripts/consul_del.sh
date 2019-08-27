284e9de3c4e398f61c8549759880be58de3e2c17
Emmanuel Duru <emmanuel.duru@atos.net>
2019-08-27 13:43:04 +0200

    Change path prefix in consul

--- src/main/resources/csar/scripts/consul_del.sh
+++ src/main/resources/csar/scripts/consul_del.sh
@@ -3,7 +3,7 @@
 echo Removing ${name} from Consul ${url}
 if [ -z "${certificate}" -o -z "${key}" ]
 then
-   curl -X DELETE ${url}/v1/kv/CU/${name}
+   curl -X DELETE ${url}/v1/kv/artemis/CU/${name}
 else
    curl -k --cert ${certificate} --key ${key} -X DELETE ${url}/v1/kv/artemis/CU/${name}
 fi
