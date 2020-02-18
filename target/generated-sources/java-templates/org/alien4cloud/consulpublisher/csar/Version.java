fc48b681e298c248c96d6a7fda857a1c0e83f239
Xavier DEGENNE <xavier.degenne@atos.net>
2020-02-18 07:14:13 +0000

    Release 2.2.0-SM11

---
+++ target/generated-sources/java-templates/org/alien4cloud/consulpublisher/csar/Version.java
@@ -0,0 +1,8 @@
+package org.alien4cloud.plugin.consulpublisher.csar;
+
+/**
+ * Contains plugin version.
+ */
+public class Version {
+    public static final String CONSULPUBLISHER_CSAR_VERSION = "2.2.0-SM11";
+}
