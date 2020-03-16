c1bf8bf875cd588c332c999e07b00bbda85c8e76
Xavier DEGENNE <xavier.degenne@gmail.com>
2020-03-16 16:04:05 +0100

    [ALIEN-3405] fix version of CSARs

--- src/main/java-templates/org/alien4cloud/consulpublisher/csar/Version.java
+++ src/main/java-templates/org/alien4cloud/consulpublisher/csar/Version.java
@@ -4,5 +4,5 @@ package org.alien4cloud.plugin.consulpublisher.csar;
  * Contains plugin version.
  */
 public class Version {
-    public static final String CONSULPUBLISHER_CSAR_VERSION = "${project.version}";
+    public static final String CONSULPUBLISHER_CSAR_VERSION = "${org.alien4cloud.consulpublisher.csar.version}";
 }
